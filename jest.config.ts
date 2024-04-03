4c1f8655f9279ad35ff78aab0c661e1048205413
Eric Fuller <info@glassmoonmedia.com>
2024-04-02 20:08:08 -0400

    Convert over to a monorepo
    - Configure jest
    - Configure a few script commands

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  projects: [
+    {
+      displayName: 'api',
+      transform: {
+        '^.+\\.ts?$': 'ts-jest',
+      },
+      testEnvironment: 'node',
+      testMatch: ['**/@(src|tests)/**/*.@(test|spec).@(ts|tsx)'],
+      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+      rootDir: '<rootDir>/packages/api',
+    }
+  ]
+};
