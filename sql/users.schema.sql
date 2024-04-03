54fe52dbfe2d91d242c503cb66db345b2f214fb2
Eric Fuller <info@glassmoonmedia.com>
2024-04-02 20:09:49 -0400

    Remove sql folder

--- sql/users.schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  username VARCHAR(100) NOT NULL UNIQUE,
-  password VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL UNIQUE,
-  firstName VARCHAR(100),
-  lastName VARCHAR(100)
-);
--- sql/users.sql
+++
@@ -1,2 +0,0 @@
-\connect essentialist-dev
-\i /sql/users.schema.sql
