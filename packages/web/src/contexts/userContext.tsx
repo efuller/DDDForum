63864e907e8a1d6405e2b9d0dee935faf7840f34
Eric Fuller <info@glassmoonmedia.com>
2024-04-09 19:24:15 -0400

    Create a basic context

---
+++ packages/web/src/contexts/userContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useState } from "react";
+
+interface UserData {
+  userName: string;
+  email: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+const UserContext = createContext<{
+  user: UserData,
+  setUser: React.Dispatch<React.SetStateAction<UserData>>
+}| null>(null);
+
+export const useUserContext = () => {
+ const currentUserContext = useContext(UserContext)
+  if (!currentUserContext) {
+    throw new Error('useUserContext must be used within a UserProvider');
+  }
+  return currentUserContext;
+};
+
+export function UserProvider(props: React.PropsWithChildren) {
+  const [user, setUser] = useState<UserData>({
+    userName: '',
+    email: '',
+  });
+
+  return <UserContext.Provider value={{ user, setUser }}>{props.children}</UserContext.Provider>;
+}
--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,9 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
+import { router } from "@/routes.tsx";
+import { UserProvider } from "@/contexts/userContext.tsx";
+
 import './index.css'
 import './App.css';
-import { router } from "@/routes.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <UserProvider>
+    <RouterProvider router={router} />
+  </UserProvider>
 );
